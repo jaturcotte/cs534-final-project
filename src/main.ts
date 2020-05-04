3167184fd410c606ab88db5dbbf280b2144a2e8f
Joseph Petitti <jppetitti@wpi.edu>
2020-05-04 13:55:29 -0400

    add condition for timing out game

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
 import { join } from "path";
 import { HumanAgent } from "./HumanAgent";
 import { Jotto } from "./Jotto";
-import { TestAgent } from "./TestAgent";
+import { RandomAgent } from "./RandomAgent";
 
 export const WORD_BANK_PATH = join(__dirname, "../wordbank.txt");
 
 (function main(): void {
   const p1 = new HumanAgent();
-  const p2 = new TestAgent();
+  const p2 = new RandomAgent();
   const j = new Jotto(p1, p2);
   j.setUp()
     .then(() => {
       return j.startGame();
     })
     .then((val) => {
-      if (val.winner === p1) {
+      if (val.winner === null) {
+        console.log("Game ended without a winner after 1000 turns")
+      } else if (val.winner === p1) {
         console.log("P1 wins after " + val.turns + " turns");
       } else {
         console.log("P2 wins after " + val.turns + " turns");
